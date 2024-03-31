f17dd7835f9f7f6fc3d4c887e53c387d6818237a
Squaapi <squaapi@gmail.com>
2024-04-01 01:25:53 +0200

    organized pause screen spaghetti code

--- Scenes/PauseScreen.gd
+++ Scenes/PauseScreen.gd
@@ -14,36 +14,7 @@ func _ready():
 	pause_scr = $/root/Node3D/PauseScreen
 	esc_pressed = $/root/Node3D/CarCutscene/Label
 
-
-func _unhandled_input(event: InputEvent) -> void:
-	if event is InputEventKey:
-		if event.is_action_pressed("esc") and Cutscene.MenuStatus == false:
-			if pause_scr.visible == false:
-				in_menu = true
-				pause_scr.visible = true
-				Crosshair.visible = false
-				task.visible = false
-				esc_pressed.visible = false
-				get_tree().paused = true
-				Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
-			elif pause_scr.visible == true:
-				in_menu = false
-				pause_scr.visible = false
-				Crosshair.visible = true
-				esc_pressed.visible = true
-				task.visible = true
-				get_tree().paused = false
-				Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
-				
-		#if event.is_action_pressed("esc") and in_menu == true:
-			#pause_scr.visible = true
-
-
-func _on_button_esc_pressed():
-	get_tree().quit()
-
-
-func _on_button_Continue_pressed():
+func exit_pause() -> void:
 	in_menu = false
 	pause_scr.visible = false
 	Crosshair.visible = true
@@ -51,7 +22,29 @@ func _on_button_Continue_pressed():
 	task.visible = true
 	get_tree().paused = false
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	
+func enter_pause() -> void:
+	in_menu = true
+	pause_scr.visible = true
+	Crosshair.visible = false
+	task.visible = false
+	esc_pressed.visible = false
+	get_tree().paused = true
+	Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
 
+func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventKey:
+		if event.is_action_pressed("esc") and Cutscene.MenuStatus == false:
+			if pause_scr.visible == false:
+				enter_pause()
+			elif pause_scr.visible == true:
+				exit_pause()
+
+func _on_button_esc_pressed():
+	get_tree().quit()
+
+func _on_button_Continue_pressed():
+	exit_pause()
 
 func _on_visible_on_screen_notifier_monster():
 	pass # Replace with function body.
