73ac1b9de3180892b925f9c9b6a71f8e57d86f0e
wetrain <filipgamer987@gmail.com>
2024-04-01 01:19:30 +0200

    removed shit (dont know how to spenn unnececary)

--- Scenes/PauseScreen.gd
+++ Scenes/PauseScreen.gd
@@ -3,6 +3,7 @@ extends Node2D
 @onready var Cutscene := $/root/Node3D/Cutscene
 @onready var Crosshair = $/root/Node3D/TextureRect
 @onready var task = $/root/Node3D/Player/Tasks
+var esc_pressed: Label
 var in_menu: bool = false
 var pause_scr: Node2D
 
@@ -11,6 +12,7 @@ var pause_scr: Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pause_scr = $/root/Node3D/PauseScreen
+	esc_pressed = $/root/Node3D/CarCutscene/Label
 
 
 func _unhandled_input(event: InputEvent) -> void:
@@ -21,18 +23,20 @@ func _unhandled_input(event: InputEvent) -> void:
 				pause_scr.visible = true
 				Crosshair.visible = false
 				task.visible = false
+				esc_pressed.visible = false
 				get_tree().paused = true
 				Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
 			elif pause_scr.visible == true:
 				in_menu = false
 				pause_scr.visible = false
 				Crosshair.visible = true
+				esc_pressed.visible = true
 				task.visible = true
 				get_tree().paused = false
 				Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 				
-		if event.is_action_pressed("esc") and in_menu == true:
-			pause_scr.visible == true
+		#if event.is_action_pressed("esc") and in_menu == true:
+			#pause_scr.visible = true
 
 
 func _on_button_esc_pressed():
@@ -43,6 +47,7 @@ func _on_button_Continue_pressed():
 	in_menu = false
 	pause_scr.visible = false
 	Crosshair.visible = true
+	esc_pressed.visible = true
 	task.visible = true
 	get_tree().paused = false
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
