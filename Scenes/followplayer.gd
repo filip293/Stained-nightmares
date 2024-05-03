161c894d439c3fcbefe7c85a528706f2b9e0a9aa
filip293 <filipgamer987@gmail.com>
2024-05-03 15:03:13 +0200

    bob look fix

--- Scenes/followplayer.gd
+++ Scenes/followplayer.gd
@@ -1,12 +1,22 @@
 extends Node3D
 
 var player
+var reset := false
 
 func _ready():
 	player = $/root/Node3D/Player
+	await get_tree().create_timer(3).timeout
+	player.in_store = false
 
 func _process(delta):
-	if player:
+	if player.error_time == true and reset == false:
+		var direction = player.global_transform.origin - global_transform.origin
+		direction.y = 0
+		direction = direction.normalized()
+		reset = true
+	
+	
+	if player and player.in_store == true and player.error_time == true:
 		var direction = player.global_transform.origin - global_transform.origin
 		direction.y = 0
 		direction = direction.normalized()
